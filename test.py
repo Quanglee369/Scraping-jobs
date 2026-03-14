b60fadecac141af02f605d930c73229ccb230afc
Quang Le <145892384+Quanglee369@users.noreply.github.com>
2026-03-14 09:41:23 +0000

    update stuff

--- test.py
+++ test.py
@@ -19,36 +19,46 @@ def sync_fact_job_postings(df):
         conn.execute(text(f"DROP TABLE IF EXISTS {staging_table}"))
     
     # 2. Push to Staging with Batching (Fast)
-    df.to_sql(
-        staging_table, 
-        con=engine, 
-        if_exists='replace', 
-        index=False, 
-        method='multi', 
-        chunksize=1000
-    )
+    try:
+        df.to_sql(
+            staging_table, 
+            con=engine, 
+            if_exists='replace', 
+            index=False, 
+            method='multi', 
+            chunksize=1000
+        )
+        print('[sync_fact_job_postings] Successfully created staging table')
+
+    except Exception as e:
+        print(f"[sync_fact_job_postings] Error in creating staging table, error: {e}")
+        return
     
     # 3. Native MERGE (Handling Updates & Inserts)
-    merge_sql = text(f"""
-        MERGE INTO {target_table} AS target
-        USING {staging_table} AS source
-        ON target.job_id = source.job_id
-        WHEN MATCHED THEN
-            UPDATE SET 
-                target.is_expired = source.is_expired,
-                target.last_seen_id = source.last_seen_id
-        WHEN NOT MATCHED THEN
-            INSERT (job_id, job_title, is_expired, job_link, last_seen_id, location_id, label_id, emp_id, created_on_id)
-            VALUES (source.job_id, source.job_title, source.is_expired, source.job_link, source.last_seen_id, 
-                    source.location_id, source.label_id, source.emp_id, source.created_on_id)
-    """)
-    
-    with engine.begin() as conn:
-        conn.execute(merge_sql)
-        conn.execute(text(f"DROP TABLE {staging_table}"))
-    
-    print(f"Successfully synced {target_table}")
+    try:
+        merge_sql = text(f"""
+            MERGE INTO {target_table} AS target
+            USING {staging_table} AS source
+            ON target.job_id = source.job_id
+            WHEN MATCHED THEN
+                UPDATE SET 
+                    target.is_expired = source.is_expired,
+                    target.last_seen_id = source.last_seen_id
+            WHEN NOT MATCHED THEN
+                INSERT (job_id, job_title, is_expired, job_link, last_seen_id, location_id, label_id, emp_id, created_on_id)
+                VALUES (source.job_id, source.job_title, source.is_expired, source.job_link, source.last_seen_id, 
+                        source.location_id, source.label_id, source.emp_id, source.created_on_id)
+        """)
+        
+        with engine.begin() as conn:
+            conn.execute(merge_sql)
+            conn.execute(text(f"DROP TABLE {staging_table}"))
+        
+        print(f"Successfully synced {target_table}")
 
+    except Exception as e:
+        print(f'[sync_fact_job_postings] Error in merging with the main table, error {e}')
+        return
 
 def sync_fact_skill_fast(df):
     if df.empty:
@@ -68,7 +78,7 @@ def sync_fact_skill_fast(df):
         print('[sync_fact_skill_fast] Success updating staging table')
     except Exception as e:
         print(f"[sync_fact_skill_fast] Error when updating staging table, error: {e}")
-
+        return
     # 3. Fast "Not Exists" Insert
     # This query only inserts rows where the combination of skill_id AND job_id doesn't exist yet
     try:
@@ -90,7 +100,7 @@ def sync_fact_skill_fast(df):
         print(f"[sync_fact_skill_fast] Successfully synced {target_table}")
     except Exception as e:
         print(f"[sync_fact_skill_fast] Error in updating main database, error: {e}")
-
+        return
 
 def databricks_hybrid_upsert(df, target_table, unique_key, columns_to_update):
     """
@@ -99,8 +109,8 @@ def databricks_hybrid_upsert(df, target_table, unique_key, columns_to_update):
     unique_key: The column to match on (e.g., 'location_name')
     columns_to_update: List of columns to update if match found (excluding the ID)
     """
-    if df.empty:
-        return
+    if df.empty or len(columns_to_update) < 0:
+        raise ValueError('dataframe is empty or list of updated column is emtpy')
     
     staging_table = f"staging_{target_table}"
 
@@ -113,12 +123,13 @@ def databricks_hybrid_upsert(df, target_table, unique_key, columns_to_update):
         print("[databricks_hybrid_upsert] Success updating staging table")
     except Exception as e:
         print(f"[databricks_hybrid_upsert] Failed to update staging table, error: {e}")
-
+        return
     # 2. Build the MERGE query
     # Logic: Match on unique_key. If exists, update metadata. If not, insert.
     update_set_clause = ", ".join([f"target.{col} = source.{col}" for col in columns_to_update])
-    insert_cols = ", ".join(columns_to_update + [unique_key])
-    insert_vals = ", ".join([f"source.{col}" for col in columns_to_update + [unique_key]])
+    all_cols = set(columns_to_update + [unique_key])
+    insert_cols = ", ".join(all_cols)
+    insert_vals = ", ".join([f"source.{col}" for col in all_cols])
 
     try:
         merge_sql = text(f"""
@@ -138,7 +149,7 @@ def databricks_hybrid_upsert(df, target_table, unique_key, columns_to_update):
         print(f"[databricks_hybrid_upsert] Successfully synced {target_table}")
     except Exception as e:
         print("[databricks_hybrid_upsert] Fail to update main databse, error: {e}")
-
+        return
 
 url = os.getenv('DATABRICKS_DB_URL')
 engine = create_engine(url, connect_args={"base_parameters": {"query_timeout": 30}})
@@ -178,14 +189,15 @@ df5 = (df
         'emp_cleaned', 'location_name', 'label', 'label_name'
     ])
 )
-df_skill = df5[['job_id', 'skills']].explode('skills').reset_index(drop=True)
-df_skill.rename(columns = {'skills': 'skill_raw'}, inplace=True)
-df_skill['skill_raw'] = df_skill['skill_raw'].str.strip()
-df_skill_final = df_skill.merge(skill_dim_dict, on='skill_raw', how='inner').drop(columns= ['skill_raw', 'skill_cleaned'])
+
+
+df_skill_final = (df5[['job_id', 'skills']]
+                .explode('skills').reset_index(drop=True)
+                .assign(skill_raw = lambda x: x['skills'].str.strip())
+                .merge(skill_dim_dict, on = 'skill_raw', how='inner')
+                .drop(columns=['skill_raw', 'skill_cleaned', 'skills'])
+                .reset_index(drop=True)
+)
 
 df5 = df5.drop(columns = 'skills')
 
-print(df5.head(15))
-print(df5.columns)
-print(df_skill_final.head(15))
-print(df_skill_final.columns)
